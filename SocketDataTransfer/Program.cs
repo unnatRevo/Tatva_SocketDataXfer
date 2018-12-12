7ee82ad917b2cf353a3b2be791a305448b07bc15
Nigel Gup <ngup@harmonix-it.com>
2018-12-12 19:19:44 +0530

    Tatvasoft Socket Data xFer final commit.

---
+++ SocketDataTransfer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketDataTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new TcpListener(IPAddress.Parse("192.168.1.161"), 8585);
+            server.Start();
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                using (var sr = new StreamReader(client.GetStream()))
+                {
+                    Console.WriteLine("Data received : " + sr.ReadToEnd());
+                }
+            }
+        }
+    }
+}
---
+++ TCPClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient("192.168.1.161", 8585);
+            using (var sw = new StreamWriter(client.GetStream()))
+            {
+                sw.Write(System.DateTime.Now);
+            }
+        }
+    }
+}
